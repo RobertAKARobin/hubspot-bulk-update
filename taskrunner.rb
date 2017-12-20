cb8993beb059475f51a2bc6c0f2fdb1bfaa6cdee
Robin Thomas <>
2017-12-20 17:06:58 -0600

    Seems to work

--- taskrunner.rb
+++ taskrunner.rb
@@ -5,20 +5,50 @@ require 'json'
 require 'csv'
 
 begin
+	raise "You need to include HAPIKEY" if !(ENV['HAPIKEY'])
+	raise "You need to include an API ENDPOINT" if !(ENV['ENDPOINT'])
+	raise "You need to include PROPERTIES" if !(ENV['PROPERTIES'])
 	columns = ENV['PROPERTIES'].split(',')
-	if !(columns.include? 'dealId')
-		raise "You need to include dealId"
-	end
-	if !(File.file? './source.csv')
-		raise "You didn't include a source.csv file"
-	end
+	raise "You didn't include a source.csv file" if !(File.file? './source.csv')
 rescue Exception => error
 	puts error
 	exit
 end
 
-records = []
+records_to_upload = []
 CSV.foreach('./source.csv').with_index do |row, index|
 	record = {}
+	record['objectId'] = row[0]
+	record['properties'] = []
+	records_to_upload.push record
+
+	columns.each_with_index do |column_name, column_index|
+		property = {
+			name: column_name,
+			value: row[column_index + 1]
+		}
+		record['properties'].push property
+	end
 end
 
+while true
+	puts "#{records_to_upload.size} remaining"
+	if records_to_upload.size <= 0
+		break
+	end
+
+	url = "#{ENV['ENDPOINT']}?hapikey=#{ENV['HAPIKEY']}"
+	body = records_to_upload.pop(100).to_json
+	headers = {
+		'Content-Type': 'application/json'
+	}
+
+	if ENV['FOR_REAL'] == 'yes'
+		response = HTTParty.post(url, body: body, headers: headers)
+		puts response.response.code
+	else
+		puts body
+	end
+end
+
+exit
